7e664dcf12f3abebad719d8fdda5bce4ce54cd09
Brady Peterson <bpeterson1663@gmail.com>
2021-10-24 14:12:44 -0500

    feat(rules): create logic to calculate date

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,17 +19,17 @@ type rule struct {
 }
 
 var rules = []rule{
-	{ID: "1", Name: "New Year's Day", IsFixed: true, MonthOfOccurrence: 0, DayOfMonth: 1, DayOfWeek: 0, WeekOfMonth: 0},
-	{ID: "2", Name: "Martin Luther King Day", IsFixed: false, MonthOfOccurrence: 0, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 3},
-	{ID: "3", Name: "Presidents' Day", IsFixed: false, MonthOfOccurrence: 0, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 3},
-	{ID: "4", Name: "Memorial Day", IsFixed: false, MonthOfOccurrence: 1, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 5},
-	{ID: "5", Name: "Juneteenth", IsFixed: true, MonthOfOccurrence: 5, DayOfMonth: 19, DayOfWeek: 0, WeekOfMonth: 0},
-	{ID: "6", Name: "Independence Day", IsFixed: true, MonthOfOccurrence: 6, DayOfMonth: 4, DayOfWeek: 0, WeekOfMonth: 0},
-	{ID: "7", Name: "Labor Day", IsFixed: false, MonthOfOccurrence: 8, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 1},
-	{ID: "8", Name: "Columbus/Indigenous Peoples Day", IsFixed: false, MonthOfOccurrence: 9, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 2},
-	{ID: "9", Name: "Veterans Day", IsFixed: true, MonthOfOccurrence: 10, DayOfMonth: 11, DayOfWeek: 0, WeekOfMonth: 0},
-	{ID: "10", Name: "Thanksgiving Day", IsFixed: false, MonthOfOccurrence: 10, DayOfMonth: 0, DayOfWeek: 4, WeekOfMonth: 4},
-	{ID: "11", Name: "Christmas Day", IsFixed: true, MonthOfOccurrence: 11, DayOfMonth: 25, DayOfWeek: 4, WeekOfMonth: 4},
+	{ID: "1", Name: "New Year's Day", IsFixed: true, MonthOfOccurrence: 1, DayOfMonth: 1, DayOfWeek: 0, WeekOfMonth: 0},
+	{ID: "2", Name: "Martin Luther King Day", IsFixed: false, MonthOfOccurrence: 1, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 3},
+	{ID: "3", Name: "Presidents' Day", IsFixed: false, MonthOfOccurrence: 2, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 3},
+	{ID: "4", Name: "Memorial Day", IsFixed: false, MonthOfOccurrence: 5, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 5},
+	{ID: "5", Name: "Juneteenth", IsFixed: true, MonthOfOccurrence: 6, DayOfMonth: 19, DayOfWeek: 0, WeekOfMonth: 0},
+	{ID: "6", Name: "Independence Day", IsFixed: true, MonthOfOccurrence: 7, DayOfMonth: 4, DayOfWeek: 0, WeekOfMonth: 0},
+	{ID: "7", Name: "Labor Day", IsFixed: false, MonthOfOccurrence: 9, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 1},
+	{ID: "8", Name: "Columbus/Indigenous Peoples Day", IsFixed: false, MonthOfOccurrence: 10, DayOfMonth: 0, DayOfWeek: 1, WeekOfMonth: 2},
+	{ID: "9", Name: "Veterans Day", IsFixed: true, MonthOfOccurrence: 11, DayOfMonth: 11, DayOfWeek: 0, WeekOfMonth: 0},
+	{ID: "10", Name: "Thanksgiving Day", IsFixed: false, MonthOfOccurrence: 11, DayOfMonth: 0, DayOfWeek: 4, WeekOfMonth: 4},
+	{ID: "11", Name: "Christmas Day", IsFixed: true, MonthOfOccurrence: 12, DayOfMonth: 25, DayOfWeek: 4, WeekOfMonth: 4},
 }
 
 func getRules(c *gin.Context) {
@@ -43,7 +45,55 @@ func getRuleByID(c *gin.Context) {
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "rule not found"})
+}
+
+func getNextHolidayByRuleID(id string) {
+
+	var holidayRule rule
+
+	for _, rule := range rules {
+		if rule.ID == id {
+			holidayRule = rule
+		}
+	}
+	if holidayRule.IsFixed {
+		d := time.Date(2020, time.Month(holidayRule.MonthOfOccurrence), holidayRule.DayOfMonth, 0, 0, 0, 0, time.UTC)
+		year, month, day := d.Date()
+
+		fmt.Printf("year = %v\n", year)
+		fmt.Printf("month = %v\n", month)
+		fmt.Printf("day = %v\n", day)
+	} else {
+		weekCount := 0
+		d := time.Date(2020, time.Month(holidayRule.MonthOfOccurrence), 1, 0, 0, 0, 0, time.UTC)
+		for {
+			if int(d.Weekday()) == holidayRule.DayOfWeek {
+				weekCount = weekCount + 1
+				if weekCount == holidayRule.WeekOfMonth {
+					break
+				} else {
+					d = d.AddDate(0, 0, 1)
+					if int(d.Month()) != holidayRule.MonthOfOccurrence {
+						d = d.AddDate(0, 0, -7)
+						break
+					}
+				}
+			} else {
+				d = d.AddDate(0, 0, 1)
+				if int(d.Month()) != holidayRule.MonthOfOccurrence {
+					d = d.AddDate(0, 0, -7)
+					break
+				}
+			}
+
+		}
+		year, month, day := d.Date()
+
+		fmt.Printf("year = %v\n", year)
+		fmt.Printf("month = %v\n", month)
+		fmt.Printf("day = %v\n", day)
+	}
 }
 
 func postRule(c *gin.Context) {
@@ -58,6 +108,8 @@ func postRule(c *gin.Context) {
 }
 
 func main() {
+	getNextHolidayByRuleID("1")
+	getNextHolidayByRuleID("8")
 	router := gin.Default()
 	router.GET("/rules", getRules)
 	router.GET("/rules/:id", getRuleByID)
